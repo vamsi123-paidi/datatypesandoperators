0b564eb4e8a1cb2dfd4508e7abeaa5b4613e886c
Vamsi-paidi <121548071+vamsi123-paidi@users.noreply.github.com>
2024-07-12 19:56:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+//declaring and printing of dtatypes
+
+//declaration and printing of a number
+let a = 1;
+console.log(a)
+console.log(typeof a)
+
+//declaration and printing of a string
+let b = "vamsi paidi";
+console.log(b)
+console.log(typeof b)
+
+//declaration and printing of a boolean
+let c = 1;
+let d = 1;
+let e = (c===d)
+console.log(e)
+console.log(typeof e)
+
+//declaration and printing of a undefined
+let f 
+console.log(f)
+console.log(typeof f)
+
+//declaration and printing of a null
+let g = null;
+console.log(g)
+console.log(typeof g)
+
+// Mathematical operators
+//basic arthematic operators
+//Addition operator
+let h=12;
+let i=13;
+console.log("Addition ",h+i)
+//Subtration opertor
+console.log("Subtraction ",h-i)
+//Multiplication operator
+console.log("Multiplication ",h*i)
+//Division operator
+console.log("Division ",h/i)
+//Modulo division 
+console.log("Modulo division ",h+i)
+
+//comparator operators
+let j=10;
+let k="10";
+//== operator
+console.log("Equal operator ",j==k)
+//=== operator
+console.log("Equal operator(strict) ",j===k)
+//!= operator
+console.log("Not equal to",j!=k)
+//!== operator
+console.log("Not equal to(strict)",j!==k)
+//> operator
+let l=12;
+let m=13;
+console.log("greater than ",l>m)
+//< operator
+console.log("Less  than ",l<m)
+//>= operator
+console.log("greater than or equals to ",l>=m)
+//<= operator
+console.log("greater than or equals to  ",l<=m)
